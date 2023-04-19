8600a48306daea71cf3eb0f0e17adf1a6c6d70d2
louisyeee <13520063@std.stei.itb.ac.id>
2023-04-20 00:27:05 +0700

    feat: help menu

--- src/index.js
+++ src/index.js
@@ -480,3 +480,14 @@ webgl.drawArticulated(state)
 // document
 //   .getElementById('stop_animate')
 //   .addEventListener('click', stopAnimation);
+
+var modal = document.getElementById('modal')
+var btn = document.getElementById('help-btn')
+var span = document.getElementsByClassName('close')[0]
+btn.onclick = function () {
+  modal.style.display = 'block'
+}
+span.onclick = function () {
+  modal.style.display = 'none'
+}
+
