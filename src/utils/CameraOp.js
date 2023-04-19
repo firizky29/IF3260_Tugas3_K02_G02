631cd5bf1168397a5fa684901dd91cc54697706f
leocardhio <leocardhio@gmail.com>
2023-04-19 10:28:00 +0700

    [fix] import issue, fix filename

--- src/utils/CameraOp.js
+++ src/utils/CameraOp.js
@@ -1,33 +1,33 @@
-import Matrix from './Matrix.js';
-import MathOp from './MathOp.js';
-import MatrixOp from './MatrixOp.js';
+import Matrix from './matrix/Matrix.js';
+import VectorOp from './VectorOp.js';
+import MatrixOp from './matrix/MatrixOp.js';
 
 const CameraOp = {
-    lookAt(cameraPos, target, up) {
-        const zAxis = MathOp.normalize(
-            MatrixOp.subtract(cameraPos, target).flatten()
-        );
-        const xAxis = MathOp.normalize(MathOp.cross(up.flatten(), zAxis));
-        const yAxis = MathOp.normalize(MathOp.cross(zAxis, xAxis));
-        cameraPos = cameraPos.flatten();
+  lookAt(cameraPos, target, up) {
+    const zAxis = VectorOp.normalize(
+      MatrixOp.subtract(cameraPos, target).flatten()
+    );
+    const xAxis = VectorOp.normalize(VectorOp.cross(up.flatten(), zAxis));
+    const yAxis = VectorOp.normalize(VectorOp.cross(zAxis, xAxis));
+    cameraPos = cameraPos.flatten();
 
-        return new Matrix(4, 4, [
-            [xAxis[0], xAxis[1], xAxis[2], 0],
-            [yAxis[0], yAxis[1], yAxis[2], 0],
-            [zAxis[0], zAxis[1], zAxis[2], 0],
-            [cameraPos[0], cameraPos[1], cameraPos[2], 1],
-        ]);
-    },
-    perspective(fovInRad, aspect, near, far) {
-        const fov = Math.tan(Math.PI * 0.5 - 0.5 * fovInRad);
-        const rangeInv = 1 / (near - far);
-        return new Matrix(4, 4, [
-            [fov / aspect, 0, 0, 0],
-            [0, fov, 0, 0],
-            [0, 0, (near + far) * rangeInv, -1],
-            [0, 0, near * far * rangeInv * 2, 0],
-        ]);
-    },
+    return new Matrix(4, 4, [
+      [xAxis[0], xAxis[1], xAxis[2], 0],
+      [yAxis[0], yAxis[1], yAxis[2], 0],
+      [zAxis[0], zAxis[1], zAxis[2], 0],
+      [cameraPos[0], cameraPos[1], cameraPos[2], 1],
+    ]);
+  },
+  perspective(fovInRad, aspect, near, far) {
+    const fov = Math.tan(Math.PI * 0.5 - 0.5 * fovInRad);
+    const rangeInv = 1 / (near - far);
+    return new Matrix(4, 4, [
+      [fov / aspect, 0, 0, 0],
+      [0, fov, 0, 0],
+      [0, 0, (near + far) * rangeInv, -1],
+      [0, 0, near * far * rangeInv * 2, 0],
+    ]);
+  },
 };
 
 export default CameraOp;
--- src/utils/Render.js
+++ src/utils/Render.js
@@ -1,19 +1,19 @@
-import MathOp from "./MathOp.js"
+import VectorOp from './VectorOp.js';
 
 const Render = {
-    rectangle(vertices, colors, normals, corners, rectColor){
-        // corners are defined counter clock-wise
-        // find normal of the rectangle
-        const normal = MathOp.triangleNormal(corners[0], corners[1], corners[2])
-        const indices = [0, 1, 2, 0, 2, 3]
-        for (let i of indices) {
-            for (let j = 0; j < 3; j++) {
-                vertices.push(corners[i][j])
-                colors.push(rectColor[j])
-                normals.push(normal[j])
-            }
-        }
+  rectangle(vertices, colors, normals, corners, rectColor) {
+    // corners are defined counter clock-wise
+    // find normal of the rectangle
+    const normal = VectorOp.triangleNormal(corners[0], corners[1], corners[2]);
+    const indices = [0, 1, 2, 0, 2, 3];
+    for (let i of indices) {
+      for (let j = 0; j < 3; j++) {
+        vertices.push(corners[i][j]);
+        colors.push(rectColor[j]);
+        normals.push(normal[j]);
+      }
     }
-}
+  },
+};
 
-export default Render
+export default Render;
